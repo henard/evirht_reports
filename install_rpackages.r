bc39300a13c5624c8db0441dec415a924aef2a35
henard <henardburrond@gmail.com>
2017-06-11 08:44:11 +0100

    b_46w - Incorporated font changes and added repos to install_packages()

--- install_rpackages.r
+++ install_rpackages.r
@@ -1,5 +1,5 @@
 # Install required packages as necesssary
-required_packages = c("RODBC", "data.table", "plyr", "ggplot2", "scales", "lazyeval")
+required_packages = c("RODBC", "data.table", "plyr", "ggplot2", "scales", "lazyeval", "extrafont")
 for(package in required_packages) {
-    if(!(package %in% installed.packages()[,"Package"])) install.packages(package)  
+    if(!(package %in% installed.packages()[,"Package"])) install.packages(package, repos='https://www.stats.bris.ac.uk/R/')  
 }
